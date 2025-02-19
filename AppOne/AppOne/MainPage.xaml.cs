47e86ef5954a211b034d47e873d16db15efe1855
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-02-19 11:13:26 -0300

    Projeto finalizado. Aula 18/02/25

---
+++ AppOne/AppOne/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppOne
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTransformers_Clicked(object sender, EventArgs e)
+        {
+            //btnTransformers.Text = "Mudou";
+
+            Button bt = (Button)sender;
+            bt.Text = "Mudou";
+
+            foto.Source = "android2";
+        }
+    }
+}
